b590d824ced627a5b3ba3a613dbf1ea51a17e865
Dawid Parafinski <dawid.parafinski@ibexa.co>
2026-09-15 10:28:51 +0200

    Replaced XML service configuration with PHP and allowed Symfony 8

--- DependencyInjection/HautelookTemplatedUriExtension.php
+++ DependencyInjection/HautelookTemplatedUriExtension.php
@@ -2,10 +2,10 @@
 
 namespace Hautelook\TemplatedUriBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -17,9 +17,9 @@ class HautelookTemplatedUriExtension extends Extension
     public function load(array $configs, ContainerBuilder $container): void
     {
         $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
+        $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
+        $loader = new PhpFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.php');
     }
 }
